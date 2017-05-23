23b1c3d18f67f5dcfe90bf5c0b6a1372ddd14273
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2017-05-23 16:34:05 -0300

    Adding test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -4,6 +4,7 @@ const ioRouter = require('../index')
 
 describe('socket.io-topic-router', () => {
   let server
+  let client
   beforeEach(() => {
     server = io(10066)
   })
@@ -14,13 +15,10 @@ describe('socket.io-topic-router', () => {
       expect(params.bar).toEqual('1234')
       expect(arg1).toEqual('arg1')
       expect(arg2).toEqual('arg2')
-
-      client.close()
-      server.close()
       done()
     })
 
-    let client = ioClient('http://localhost:10066')
+    client = ioClient('http://localhost:10066')
     client.emit('foo/1234', 'arg1', 'arg2')
   })
 
@@ -29,7 +27,6 @@ describe('socket.io-topic-router', () => {
     server.use(router)
     router.route('foo/:bar', () => {
       fail('Should not be called')
-      done()
     })
 
     router.route('foo/:bar', () => {
@@ -49,14 +46,32 @@ describe('socket.io-topic-router', () => {
 
     router.route('fo0/:bar', (params, arg1, arg2) => {
       fail('Should not be called')
-      done()
     })
 
-    let client = ioClient('http://localhost:10066')
+    client = ioClient('http://localhost:10066')
     client.emit('foo/1234', 'arg1', 'arg2')
   })
 
+  it('should still support socket.io events', (done) => {
+    let router = ioRouter()
+    server.use(router)
+    router.route('foo/:bar', (params, arg1, arg2) => {
+      fail('Should not be called')
+    })
+
+    server.on('connection', socket => {
+      socket.on('message', msg => {
+        expect(msg).toEqual('foo/1234')
+        done()
+      })
+    })
+
+    client = ioClient('http://localhost:10066')
+    client.send('foo/1234')
+  })
+
   afterEach(() => {
+    client && client.close()
     server.close()
   })
 })
